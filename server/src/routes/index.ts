5135aa999fb5517d0700f270e940576015c1afa6
Christian Blom <christian@christianblom.se>
2020-07-02 20:01:02 +0200

    Updated dependencies and started removing unused code.

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import UserRouter from './Users';
 import AuthRouter from './Auth';
-import PushRouter from './PushNotifications';
-import SubscribersRouter from './Subscribers';
 
 // Init router and path
 const router = Router();
@@ -10,8 +8,6 @@ const router = Router();
 // Add sub-routes
 router.use('/users', UserRouter);
 router.use('/auth', AuthRouter);
-router.use('/push', PushRouter);
-router.use('/subscribers', SubscribersRouter);
 
 // Export the base-router
 export default router;
